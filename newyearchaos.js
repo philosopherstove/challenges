1f68669886a992318c921da5c37f7443052b78a4
Steven D'Arolfi <stevendarolfi@hotmail.com>
2020-05-16 04:56:21 -0400

    init solution

--- newyearchaos.js
+++ newyearchaos.js
@@ -12,7 +12,6 @@ Any person in the queue can bribe the person directly in front of them to swap p
 If two people swap positions, they still wear the same sticker denoting their original places in line.
 One person can bribe at most two others.
 For example, if n = 8 and Person 5 bribes Person 4, the queue will look like this: 1,2,3,5,4,6,7,8.
-
 Fascinated by this chaotic queue, you decide you must know the minimum number of bribes that took place to get the queue into its current state!
 
 Function Description:
